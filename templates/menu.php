7dc60cd7932804c043e572484db1c4e6b3725004
PN <pn@klapproth-koch.de>
2023-04-26 10:29:44 +0200

    Menu Styling Änderungen

--- templates/menu.php
+++ templates/menu.php
@@ -11,7 +11,7 @@
     </div>
 </div>
 
-<div class="menu-list">
+<div class="menu-list-desktop">
     <div class="menu-top">
         <div class="navigation-left">
             {{insert_module::89}}
@@ -21,9 +21,52 @@
         </div>
         <div class="navigation-right">
             {{insert_module::91}}
+            {{insert_module::99}}
         </div>
     </div>
     <div class="menu-bottom">
         {{insert_module::84}}
     </div>
 </div>
+
+<nav class="mod_navigation-mobile">
+    <ul class="menu-list">
+        <li>
+            <section class="ce_accordionSingle ce_accordion ce_text block">
+                <div class="toggler ui-accordion-header ui-corner-top ui-state-default ui-accordion-icons " role="tab" id="ui-id-1" aria-controls="ui-id-2" tabindex="0">
+                    <span>Nur bei Roth</span></div>
+                <div class="accordion ui-accordion-content ui-corner-bottom ui-helper-reset ui-widget-content ui-accordion-content-active" style="display: block;" id="ui-id-2" aria-labelledby="ui-id-1" role="tabpanel" aria-hidden="false">
+                    <div>
+                        {{insert_module::98}}
+                    </div>
+                </div>
+            </section>
+        </li>
+        <li>
+            <section class="ce_accordionSingle ce_accordion ce_text block">
+                <div class="toggler ui-accordion-header ui-corner-top ui-state-default ui-accordion-icons " role="tab" id="ui-id-1" aria-controls="ui-id-2" tabindex="0">
+                    <span> Deine Karriere</span></div>
+                <div class="accordion ui-accordion-content ui-corner-bottom ui-helper-reset " style="display: block;" id="ui-id-2" aria-labelledby="ui-id-1" role="tabpanel" aria-hidden="false">
+                    <div>
+                        {{insert_module::97}}
+                    </div>
+                </div>
+            </section>
+        </li>
+        <li>
+
+            <section class="ce_accordionSingle ce_accordion ce_text block">
+                <div class="toggler ui-accordion-header ui-corner-top ui-state-default ui-accordion-icons " role="tab" id="ui-id-1" aria-controls="ui-id-2" tabindex="0">
+                    <span>Unser Unternehmen</span></div>
+                <div class="accordion ui-accordion-content ui-corner-bottom ui-helper-reset " style="display: block;" id="ui-id-2" aria-labelledby="ui-id-1" role="tabpanel" aria-hidden="false">
+                    <div>
+                        {{insert_module::98}}
+                    </div>
+                </div>
+            </section>
+        </li>
+    </ul>
+    <div class="menu-bottom">
+        {{insert_module::84}}
+    </div>
+</nav>
