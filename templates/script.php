4fd1d6bd0d96a748a7ea7475719524dd882b0aa9
SamesOfAm <flo.tepelmann@ewetel.net>
2023-05-04 15:46:22 +0200

    Showroom, Language Switcher

--- templates/script.php
+++ templates/script.php
@@ -1,4 +1,5 @@
 <script>
+  if(document.getElementById('icon_1')) {
     const icon_1 = document.getElementById('icon_1');
     const icon_2 = document.getElementById('icon_2');
     const icon_3 = document.getElementById('icon_3');
@@ -14,45 +15,37 @@
     console.log(foo);
 
 
+    if (icon_1.href === String(document.location) || spl(icon_1) === String(foo[0])) {
+      icon_1.getElementsByTagName('img')[0].src = "files/layout/showroom_icons/interieur_blue.svg";
+      $('#icon_1').removeClass('effect');
+    } else if (icon_2.href === String(document.location) || spl(icon_2) === String(foo[0])) {
+      icon_2.getElementsByTagName('img')[0].src = "files/layout/showroom_icons/display_blue.svg";
+      $('#icon_2').removeClass('effect');
+    } else if (icon_3.href === String(document.location) || spl(icon_3) === String(foo[0])) {
+      icon_3.getElementsByTagName('img')[0].src = "files/layout/showroom_icons/lager_blue.svg";
+      $('#icon_3').removeClass('effect');
+    } else if (icon_4.href === String(document.location) || spl(icon_4) === String(foo[0])) {
+      icon_4.getElementsByTagName('img')[0].src = "files/layout/showroom_icons/energie_blue.svg";
+      $('#icon_4').removeClass('effect');
+    } else if (icon_5.href === String(document.location) || spl(icon_5) === String(foo[0])) {
+      icon_5.getElementsByTagName('img')[0].src = "files/layout/showroom_icons/consumer_blue.svg";
+      $('#icon_5').removeClass('effect');
+    } else if (icon_6.href === String(document.location) || spl(icon_6) === String(foo[0])) {
+      icon_6.getElementsByTagName('img')[0].src = "files/layout/showroom_icons/dichtungssysteme_blue.svg";
+      $('#icon_6').removeClass('effect');
+    } else if (icon_7.href === String(document.location) || spl(icon_7) === String(foo[0])) {
+      icon_7.getElementsByTagName('img')[0].src = "files/layout/showroom_icons/lueftung_blue.svg";
+      $('#icon_7').removeClass('effect');
+    } else if (icon_8.href === String(document.location) || spl(icon_8) === String(foo[0])) {
+      icon_8.getElementsByTagName('img')[0].src = "files/layout/showroom_icons/health_blue.svg";
+      $('#icon_8').removeClass('effect');
+    }
 
-        if (icon_1.href === String(document.location) || spl(icon_1) === String(foo[0])){
-            icon_1.getElementsByTagName('img')[0].src="files/layout/showroom_icons/interieur_blue.svg";
-            $('#icon_1').removeClass('effect');
-         }
-        else if  (icon_2.href === String(document.location) || spl(icon_2) === String(foo[0])){
-            icon_2.getElementsByTagName('img')[0].src="files/layout/showroom_icons/display_blue.svg";
-            $('#icon_2').removeClass('effect');
-         }
-        else if  (icon_3.href === String(document.location) || spl(icon_3) === String(foo[0])){
-            icon_3.getElementsByTagName('img')[0].src="files/layout/showroom_icons/lager_blue.svg";
-            $('#icon_3').removeClass('effect');
-        }
-        else if  (icon_4.href === String(document.location) || spl(icon_4) === String(foo[0])){
-            icon_4.getElementsByTagName('img')[0].src="files/layout/showroom_icons/energie_blue.svg";
-            $('#icon_4').removeClass('effect');
-        }
-        else if  (icon_5.href === String(document.location) || spl(icon_5) === String(foo[0])){
-            icon_5.getElementsByTagName('img')[0].src="files/layout/showroom_icons/consumer_blue.svg";
-            $('#icon_5').removeClass('effect');
-        }
-        else if  (icon_6.href === String(document.location) || spl(icon_6) === String(foo[0])){
-            icon_6.getElementsByTagName('img')[0].src="files/layout/showroom_icons/dichtungssysteme_blue.svg";
-            $('#icon_6').removeClass('effect');
-        }
-        else if  (icon_7.href === String(document.location) || spl(icon_7) === String(foo[0])){
-            icon_7.getElementsByTagName('img')[0].src="files/layout/showroom_icons/lueftung_blue.svg";
-            $('#icon_7').removeClass('effect');
-        }
-
-        else  if (icon_8.href === String(document.location) || spl(icon_8) === String(foo[0])){
-            icon_8.getElementsByTagName('img')[0].src="files/layout/showroom_icons/health_blue.svg";
-            $('#icon_8').removeClass('effect');
-        }
-
-        function spl(x) {
-                let q = x.href.split('#');
-                return String(q[0]);
-        }
+    function spl(x) {
+      let q = x.href.split('#');
+      return String(q[0]);
+    }
+  }
 </script>
 
 <script>
---
+++ templates/showroom.php
@@ -0,0 +1,20 @@
+<div class="submenu_showroom">
+  <div class="box">
+    <a class="effect" id="icon_1" href="{{link_url::125|urlattr}}#main-article"><img src="files/layout/showroom_icons/interieur.svg"><span>Interieur & <br>Exterieur</span></a>
+    <a class="effect" id="icon_4" href="{{link_url::129|urlattr}}#main-article"><img src="files/layout/showroom_icons/energie.svg"><span>Energie & Elektro</span></a>
+  </div>
+  <div class="box">
+    <a class="effect" id="icon_2" href="{{link_url::127|urlattr}}#main-article"><img src="files/layout/showroom_icons/display.svg"><span>Display & <br>Lichttechnik</span></a>
+    <a class="effect" id="icon_8" href="{{link_url::133|urlattr}}#main-article"><img src="files/layout/showroom_icons/health.svg"><span>Health Care</span></a>
+
+
+  </div>
+  <div class="box">
+    <a class="effect" id="icon_3" href="{{link_url::128|urlattr}}#main-article"><img src="files/layout/showroom_icons/lager.svg"><span>Lager & Logistik</span></a>
+    <a class="effect" id="icon_5" href="{{link_url::130|urlattr}}#main-article"><img src="files/layout/showroom_icons/consumer.svg"><span>Consumer Goods</span></a>
+  </div>
+  <div class="box">
+    <a class="effect" id="icon_6" href="{{link_url::131|urlattr}}#main-article"><img src="files/layout/showroom_icons/dichtungssysteme.svg"><span>Dichtungssysteme</span></a>
+    <a class="effect" id="icon_7" href="{{link_url::132|urlattr}}#main-article"><img src="files/layout/showroom_icons/lueftung.svg"><span>Lüftung & Klima</span></a>
+  </div>
+</div>
