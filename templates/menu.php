47823b53d71d5d2dbfe6a0b5e2e908002aa33453
SamesOfAm <flo.tepelmann@ewetel.net>
2023-05-09 17:34:48 +0200

    Bugfixing mobile

--- templates/menu.php
+++ templates/menu.php
@@ -3,11 +3,11 @@
         {{insert_content::4641}}
     </div>
     <div class="header-bottom">
-        {{insert_module::92}}
         <div class="header-bottom-right">
             {{insert_content::4488}}
             {{insert_module::88}}
         </div>
+        {{insert_module::92}}
     </div>
 </div>
 
@@ -32,38 +32,16 @@
 <nav class="mod_navigation-mobile">
     <ul class="menu-list">
         <li>
-            <section class="ce_accordionSingle ce_accordion ce_text block">
-                <div class="toggler ui-accordion-header ui-corner-top ui-state-default ui-accordion-icons " role="tab" id="ui-id-1" aria-controls="ui-id-2" tabindex="0">
-                    <span>Nur bei Roth</span></div>
-                <div class="accordion ui-accordion-content ui-corner-bottom ui-helper-reset ui-widget-content ui-accordion-content-active" style="display: block;" id="ui-id-2" aria-labelledby="ui-id-1" role="tabpanel" aria-hidden="false">
-                    <div>
-                        {{insert_module::96}}
-                    </div>
-                </div>
-            </section>
+            <div>Nur bei Roth</div>
+            {{insert_module::96}}
         </li>
         <li>
-            <section class="ce_accordionSingle ce_accordion ce_text block">
-                <div class="toggler ui-accordion-header ui-corner-top ui-state-default ui-accordion-icons " role="tab" id="ui-id-1" aria-controls="ui-id-2" tabindex="0">
-                    <span> Deine Karriere</span></div>
-                <div class="accordion ui-accordion-content ui-corner-bottom ui-helper-reset " style="display: block;" id="ui-id-2" aria-labelledby="ui-id-1" role="tabpanel" aria-hidden="false">
-                    <div>
-                        {{insert_module::97}}
-                    </div>
-                </div>
-            </section>
+            <div>Deine Karriere</div>
+            {{insert_module::97}}
         </li>
         <li>
-
-            <section class="ce_accordionSingle ce_accordion ce_text block">
-                <div class="toggler ui-accordion-header ui-corner-top ui-state-default ui-accordion-icons " role="tab" id="ui-id-1" aria-controls="ui-id-2" tabindex="0">
-                    <span>Unser Unternehmen</span></div>
-                <div class="accordion ui-accordion-content ui-corner-bottom ui-helper-reset " style="display: block;" id="ui-id-2" aria-labelledby="ui-id-1" role="tabpanel" aria-hidden="false">
-                    <div>
-                        {{insert_module::98}}
-                    </div>
-                </div>
-            </section>
+            <div>Unser Unternehmen</div>
+            {{insert_module::98}}
         </li>
     </ul>
     <div class="menu-bottom">
