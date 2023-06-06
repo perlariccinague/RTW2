a0c26b46f0762c4c86d8a80493adc1525fb12988
PN <pn@klapproth-koch.de>
2023-06-06 15:50:23 +0200

    404-Seite, Footer-icon, menu englische

---
+++ templates/menu_en.php
@@ -0,0 +1,50 @@
+<div class="header">
+    <div class="header-top">
+        {{insert_content::4641}}
+    </div>
+    <div class="header-bottom">
+        <div class="header-bottom-right">
+            {{insert_content::4488}}
+            {{insert_module::88}}
+        </div>
+        {{insert_module::92}}
+    </div>
+</div>
+
+<div class="menu-list-desktop">
+    <div class="menu-top">
+        <div class="navigation-left">
+            {{insert_module::106}}
+        </div>
+        <div class="navigation-middle">
+            {{insert_module::114}}
+        </div>
+        <div class="navigation-right">
+            {{insert_module::107}}
+            {{insert_module::110}}
+        </div>
+    </div>
+    <div class="menu-bottom">
+        {{insert_module::84}}
+    </div>
+</div>
+
+<nav class="mod_navigation-mobile">
+    <ul class="menu-list">
+        <li>
+            <div>Only with Roth</div>
+            {{insert_module::108}}
+        </li>
+        <li>
+            <div>Deine Karriere</div>
+            {{insert_module::115}}
+        </li>
+        <li>
+            <div>Our company</div>
+            {{insert_module::109}}
+        </li>
+    </ul>
+    <div class="menu-bottom">
+        {{insert_module::84}}
+    </div>
+</nav>
