dd189a38bf32f1c2204ab7805bd72a00239407ea
Florian <flo.tepelmann@ewetel.net>
2023-09-14 17:18:12 +0200

    Polish Menu Template and Footer Styling

---
+++ templates/menu_pl.php
@@ -0,0 +1,49 @@
+<div class="header">
+    <div class="header-top">
+        {{insert_content::4641}}
+    </div>
+    <div class="header-bottom">
+        <div class="header-bottom-right">
+            {{insert_content::4488}}
+            {{insert_module::88}}
+        </div>
+        {{insert_module::92}}
+    </div>
+</div>
+
+<div class="menu-list-desktop">
+    <div class="menu-top">
+        <div class="navigation-left">
+            {{insert_module::122}}
+        </div>
+        <div class="navigation-middle">
+            {{insert_module::123}}
+        </div>
+        <div class="navigation-right">
+            {{insert_module::124}}
+        </div>
+    </div>
+    <div class="menu-bottom">
+        {{insert_module::84}}
+    </div>
+</div>
+
+<nav class="mod_navigation-mobile">
+    <ul class="menu-list">
+        <li>
+            <div>Tylko w roth</div>
+            {{insert_module::130}}
+        </li>
+        <li>
+            <div>Deine Karriere</div>
+            {{insert_module::131}}
+        </li>
+        <li>
+            <div>Nasza firma</div>
+            {{insert_module::132}}
+        </li>
+    </ul>
+    <div class="menu-bottom">
+        {{insert_module::84}}
+    </div>
+</nav>
